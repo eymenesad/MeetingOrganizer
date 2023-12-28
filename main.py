680c6e0c862973cc72e17eb403b8a9097e06ae2e
eymenesad <eymenesad123@gmail.com>
2023-12-28 16:24:21 +0300

    aa

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from flask import Flask
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return 'Meeting Organizer Ana Sayfası'
+
+@app.route('/create-meeting', methods=['GET', 'POST'])
+def create_meeting():
+    if request.method == 'POST':
+        # Form verilerini burada işleyebilirsiniz
+        topic = request.form['topic']
+        date = request.form['date']
+        start_time = request.form['start_time']
+        end_time = request.form['end_time']
+        participants = request.form['participants']
+
+        # Verileri işledikten sonra, örneğin veritabanına kaydedebilirsiniz
+
+        return 'Toplantı başarıyla kaydedildi'
+    else:
+        return render_template('meeting_form.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
